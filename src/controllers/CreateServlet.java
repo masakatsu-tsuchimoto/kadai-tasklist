d408b6dc7955579f9d0b999d45b443d7ecac02cf
masakatsu.tsuchimoto <laugh.wincy@gmail.com>
2022-01-13 15:25:50 +0900

    バリデーションを追加

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -70,14 +70,6 @@ public class CreateServlet extends HttpServlet {
                 // indexのページにリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
             }
-
-            // データベースに保存
-            em.persist(m);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");       // ここを追記
-            em.close();
-
-            response.sendRedirect(request.getContextPath() + "/index");
         }
     }
 
